31132403ecc461fbeb35fdc8aa38c47e0129fd9a
Miary <miar0004@stud.kea.dk>
2020-10-07 05:25:52 +0200

    countdown JS

---
+++ app.js
@@ -0,0 +1,32 @@
+function startTimer(){
+    let countDownDate = new Date(select("#date").value+"T"+select("#time").value).getTime();
+
+    // Update the count down every 1 second
+    let x = setInterval(function() {
+
+        let now = new Date().getTime();
+        let distance = countDownDate - now;
+            
+        // Time calculations for days, hours, minutes and seconds
+        let months=Math.floor(distance / (1000 * 60 * 60 * 24 * 30));
+        let days = Math.floor(distance % (1000 * 60 * 60 * 24 * 30) / (1000 * 60 * 60 * 24) );
+        let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+            
+        select(".months h1").innerText=months;
+        select(".days h1").innerText=days;
+        select(".hours h1").innerText=hours;
+        select(".minutes h1").innerText=minutes;
+        select(".seconds h1").innerText=seconds;
+            
+        // If the count down is over, write some text 
+        if (distance < 0) {
+            clearInterval(x);
+        }
+    }, 1000);
+}
+
+function select(element){
+    return document.querySelector(element);
+}
